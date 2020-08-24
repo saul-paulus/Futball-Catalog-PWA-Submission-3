37a0b0935a27111358fd8295a6cd5d8d16128209
saul-paulus <saulpaulus17@icloud.com>
2020-08-24 15:25:00 +0900

    hapus scope

--- assets/js/swRegister.js
+++ assets/js/swRegister.js
@@ -1,7 +1,7 @@
 // REGISTER SERVICE WORKER
 const registerServiceWorker = () => {
   return navigator.serviceWorker
-    .register("./serviceWorker.js", { scope: "./" })
+    .register("./serviceWorker.js")
     .then((registration) => {
       console.log("registrasi service worker berhasil", registration);
       return registration;
